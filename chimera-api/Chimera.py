22e1099e9f774858250b95258ff47b3cefaea4fe
Daniel Kudrow <dkudrow@cs.ucsb.edu>
2014-11-19 22:19:49 -0800

    restructure api

---
+++ chimera-api/Chimera.py
@@ -0,0 +1,24 @@
+class Chimera:
+    def handleWithdraw(self, amount):
+        return 'ok'
+
+    def handleDeposit(self, amount):
+        return 'ok'
+
+    def handleBalance(self):
+        return 'ok'
+
+    def handleFail(self):
+        return 'ok'
+
+    def handleUnfail(self):
+        return 'ok'
+
+    def handlePrepare(self):
+        return 'ok'
+
+    def handleAccept(self):
+        return 'ok'
+
+    def handleElection(self):
+        return 'ok'
--- chimera-api/Listen.py
+++ chimera-api/Listen.py
@@ -1,24 +1,67 @@
 #!env/bin/python
+# Listen.py -- RESTful API for Chimera
 
 import json
 import flask
+import Chimera
 
+chimera = Chimera.Chimera()
 app = flask.Flask(__name__)
 
 @app.route('/')
 def alive():
     return 'Chimera node is running'
 
-@app.route('/message', methods=['POST'])
-def recv_message():
-    params = flask.request.form
+# Public API
+@app.route('/withdraw/<amount>')
+def withdraw(amount):
+    status = chimera.handleWithdraw(int(amount))
+    return json.dumps({'status':status})
+
+@app.route('/deposit/<amount>')
+def deposit(amount):
+    status = chimera.handleDeposit(int(amount))
+    return json.dumps({'status':status})
+
+@app.route('/balance')
+def balance():
+    status = chimera.handleBalance()
+    return json.dumps({'status':status})
+
+@app.route('/fail')
+def fail():
+    status = chimera.handleFail()
+    return json.dumps({'status':status})
+
+@app.route('/unfail')
+def unfail():
+    status = chimera.handleUnfail()
+    return json.dumps({'status':status})
+
+# Internal Paxos messages
+@app.route('/prepare')
+def prepare():
+    params = flask.request.args
     try:
-        msg_type = params['msg_type']
-        sender = params['sender']
-        msg = params['msg']
-        return 'msg_type: %s\nsender: %s\nmsg: %s\n' % (msg_type, sender, msg)
-    except KeyError, e:
-        return 'Invalid parameters'
+        status = chimera.handlePrepare()
+    except KeyError:
+        status = 'invalid_params'
+    return json.dumps({'status':status})
+
+@app.route('/accept')
+def accept():
+    params = flask.request.args
+    try:
+        status = chimera.handleAccept()
+    except KeyError:
+        status = 'invalid_params'
+    return json.dumps({'status':status})
+
+# Internal leader election messages
+@app.route('/election')
+def election():
+    status = chimera.handleElection()
+    return json.dumps({'status':status})
          
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
